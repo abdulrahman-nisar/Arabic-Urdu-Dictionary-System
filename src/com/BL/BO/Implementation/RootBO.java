0930f86bb134c6d0c321ceb65241093cf1cfac62
abdulrahman-nisar <abdulrahmannisar6@gmail.com>
2025-10-19 20:49:15 +0500

    Adding facade to bl

--- src/com/BL/BO/Implementation/RootBO.java
+++ src/com/BL/BO/Implementation/RootBO.java
@@ -1,7 +1,23 @@
 package com.BL.BO.Implementation;
 
 import com.BL.BO.Interface.IRootBO;
+import com.DAL.DAO.Interface.IPatternDAO;
+import com.DAL.DAO.Interface.IRootDAO;
+import com.DAL.DAO.Interface.IWordDAO;
+import com.DAL.Facade.DALFacade;
+import com.DAL.Facade.IDALFacade;
 
 public class RootBO implements IRootBO {
 
+	private IDALFacade facade;
+	
+	public RootBO(IRootDAO rootDAO, IPatternDAO patternDAO, IWordDAO wordDAO) {
+		facade = new DALFacade(rootDAO, patternDAO, wordDAO);
+	}
+
+	@Override
+	public boolean isValidRoot(String rootLetters) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
